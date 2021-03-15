0795f687a61d8cb8a4a44b1c0790dd207cc8e618
giovannipalmieri26 <74723516+giovannipalmieri26@users.noreply.github.com>
2021-03-15 14:36:38 +0100

    Delete ristorante/demo directory

--- ristorante/demo/src/main/java/it/univpm/demo/eccezioni/EccezioneData.java
+++
@@ -1,8 +0,0 @@
-package it.univpm.demo.eccezioni;
-
-public class EccezioneData extends Exception{
-	private static final long serialVersionUID = 1L;
-	public EccezioneData() {
-		super("La data non è stata inserita correttamente. Riprovare");
-	}
-}
